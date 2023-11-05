1c5b80776af9969a9d0a39ca2eb8cd995a7f7854
Yrnar <ernar030913@gmail.com>
2023-11-06 00:22:58 +0600

    Chapter 11

--- assignment2/cmd/api/routes.go
+++ assignment2/cmd/api/routes.go
@@ -6,7 +6,7 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func (app *application) routes() *httprouter.Router {
+func (app *application) routes() http.Handler {
 	router := httprouter.New()
 	router.NotFound = http.HandlerFunc(app.notFoundResponse)
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
@@ -17,5 +17,6 @@ func (app *application) routes() *httprouter.Router {
 	router.HandlerFunc(http.MethodPatch, "/v1/plantseed/:id", app.updatePlantseedHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/plantseed/:id", app.deletePlantseedHandler)
 
-	return router
+	return app.recoverPanic(router)
+
 }
