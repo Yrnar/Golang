d2b11f3ad4430e58525d70c65ba8268ccd638640
Yrnar <ernar030913@gmail.com>
2023-12-02 22:08:31 +0600

    Chapter 18

--- assignment2/cmd/api/main.go
+++ assignment2/cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"flag"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -35,6 +36,9 @@ type config struct {
 		password string
 		sender   string
 	}
+	cors struct {
+		trustedOrigins []string
+	}
 }
 
 type application struct {
@@ -63,6 +67,11 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "5e42340570383d", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "80ccc0ace6c526", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.alexedwards.net>", "SMTP sender")
+
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 
 	flag.Parse()
 
